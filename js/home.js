88403be004740b125070c1268c7a2f6dc2a36f2f
Beckett Brees <bbreespt@gmail.com>
2021-07-27 08:04:28 -0400

    hide recipe function

--- js/home.js
+++ js/home.js
@@ -1,1 +1,9 @@
-alert ("Beginning Project");
+
+function myFunction() {
+    let x = document.getElementById("card-back-1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
