bc55798196916aecdc7099fd6ccf2c5c9fa04a84
Beckett Brees <bbreespt@gmail.com>
2021-07-29 21:25:04 -0400

    readme additions

--- js/main.js
+++ js/main.js
@@ -1,10 +1,27 @@
+
 function myValidation() {
     let x = document.forms["contact"]["fname"].value;
     if (x == "") {
       alert("Name must be filled out");
-      return false;
+    } else {
+      alert("Thank you," + x + "!");
     }
-  }
+
+    let y = document.forms["contact"]['email'].value;
+    if (y == "") {
+      alert ("we need an email to connect");
+    } else {
+      alert ("thanks for sharing");
+    }
+
+    let z = document.forms["contact"]["newRecipe"].value;
+    if (z == "") {
+      alert ("I think we missed that recipe, please put it in the box");}
+      else{
+        alert("we look forward to trying your favorites");
+      }
+    }
+  
 
   const mediaQuery = window.matchMedia("only screen and (max-width: 768px)");
 
